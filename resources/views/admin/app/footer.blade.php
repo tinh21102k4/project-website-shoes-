3fac2a1ff07b8dc968b8177dc757baad06e21d7b
hà duy tình <haduytinh21102004@gmail.com>
2024-11-13 10:23:18 +0700

    website shoes

---
+++ resources/views/admin/app/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+             © 2024 Bản quyền thuộc về:
+             <a class="text-info" href="/ ">Hà Duy Tình PH13108</a>
+        </div>
+    </div>
+ </footer>
+ 
